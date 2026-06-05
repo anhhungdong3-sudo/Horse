40d69ae81b5ad2c19a701ec5a3e9bdef51cd23d8
anhhungdong3-sudo <anhhungdong3@gmail.com>
2026-06-05 23:36:56 +0700

    Update Dice.java

--- src/GameSetup/Dice.java
+++ src/GameSetup/Dice.java
@@ -1,1 +1,23 @@
 
+package GameSetup;
+
+import java.util.Random;
+
+public class Dice {
+	private int result;
+
+	private Random random = new Random();
+
+	public int rollDice() {
+		result = random.nextInt(6) + 1;
+		return result;
+	}
+
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+}
